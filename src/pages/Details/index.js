4c2c803415b74c00427a57b9433cda53e2fe141c
Bruno Pinhal <b.seek2@gmail.com>
2020-03-29 00:21:09 -0300

    Contact buttons functions

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Feather} from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
-import { View, FlatList, Image, Text, TouchableOpacity } from 'react-native';
+import { View, Image, Text, TouchableOpacity, Linking } from 'react-native';
 import * as MailComposer from 'expo-mail-composer';
 
 import logo from '../../assets/logo.png';
@@ -24,7 +24,7 @@ export default function Incidents() {
   }
 
   function sendWhatsapp () {
-
+    Linking.openURL(`whatsapp://send?phone=0000000000000&text=${message}`);
   }
 
   return(
@@ -53,7 +53,7 @@ export default function Incidents() {
         <Text style={styles.heroDescription}>Entre em contato:</Text>
 
         <View style={styles.actions}>
-          <TouchableOpacity style={styles.action} onPress={() => {}}>
+          <TouchableOpacity style={styles.action} onPress={sendWhatsapp}>
             <Text style={styles.actionText}>WhatsApp</Text>
           </TouchableOpacity>
 
